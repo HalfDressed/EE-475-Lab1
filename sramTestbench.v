ed6c58ea6501729c831668e9c93e92bb735a6d45
Michael Von Hippel <vonhippelscienceandenterprises@gmail.com>
2016-01-12 19:54:18 -0800

    fleshed out testbench

--- sramTestbench.v
+++ sramTestbench.v
@@ -5,7 +5,7 @@ tests signals coming from controller
 */
 
 module sramTestbench():
-	read, write, clk, reset;
+	reg read, write, clk, reset;
 	wire latch, dataEnable, csBar, oeBar, weBar;
 
 	SRAM sramController(
@@ -25,7 +25,48 @@ module sramTestbench():
 	// Run through instructions to test that each function works
 	initial begin
 		clk = 0;
-		
+		reset <= 1;
+		#(Delay * 2)
+		reset <= 0;
+		#(Delay * 2) 
+
+		// Read stuff
+		read <= 1;
+		write <= 0;
+		#(Delay * 10)		
+
+		reset <= 1;
+		#(Delay * 2) 
+		reset <= 0;
+		#(Delay * 2) 
+
+		// Write stuff
+		read <= 0;
+		write <= 1;
+		#(Delay * 10)
+
+		reset <= 1;
+		#(Delay * 2) 
+		reset <= 0;
+		#(Delay * 2) 
+
+		// Should idle...
+		read <= 0;
+		write <= 0;
+		#(Delay * 10)		
+
+		reset <= 1;
+		#(Delay * 2) 
+		reset <= 0;
+		#(Delay * 2) 
+
+		// Should still idle
+		read <= 1;
+		write <= 1;
+		#(Delay * 10)
+
+		#(Delay * 2) 
+
 		#(Delay * 2);
 
 		$stop;
